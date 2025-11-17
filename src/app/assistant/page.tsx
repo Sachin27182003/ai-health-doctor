b33379dfa3d37ed065c1d82ded56a8a5c26dc2c8
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2025-11-18 03:43:13 +0530

    Added an assistant who can talk like doctor

---
+++ src/app/assistant/page.tsx
@@ -0,0 +1,47 @@
+// app/(root)/page.tsx
+
+import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+async function Home() {
+  return (
+    <>
+      <section className="card-cta border-4 caret-transparent">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Your 24/7 personal health assistant, always ready to assist.</h2>
+          <p className="text-lg">
+            Get immediate answers to your health questions, anytime, anywhere.
+          </p>
+          <span>
+            <Button
+              asChild
+              className="w-fit !bg-violet-300 !text-zinc-950 hover:!bg-violet-300/80 !rounded-full !font-bold p-5 cursor-pointer min-h-10 max-sm:w-full border-4"
+            >
+              <Link href="/assistant/interview">Start Consultation</Link>
+            </Button>
+            <Button
+              asChild
+              className="w-fit !bg-violet-300 !text-zinc-950 hover:!bg-violet-300/80 !rounded-full !font-bold p-5 cursor-pointer min-h-10 max-sm:w-full border-4"
+            >
+              <Link href="/">Back to Previous </Link>
+            </Button>
+          </span>
+        </div>
+
+        {/* <div> */}
+        <Image
+          src="/ai-doctor.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden rounded-2xl mix-blend-screen border-6 border-red-500 "
+        />
+        {/* </div> */}
+      </section>
+    </>
+  );
+}
+
+export default Home;
